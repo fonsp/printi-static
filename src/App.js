7ce014654865cb535f49e0a03323f8167fbacf14
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2019-01-27 20:13:51 +0100

    Wowww

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Printer from './components/Printer'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <Route exact path="\" component={Home} />
+        <Route path="\:printer" component={Printer} />
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Printer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Printer extends Component {
+  render() {
+    console.log('this.props', this.props)
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Printer
